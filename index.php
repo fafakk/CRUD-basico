14ef54935e0fa21ce962a98169bc9193ff6c0f08
Fafa <esteban.galfrascoli@hotmail.com>
2021-02-11 14:57:07 -0300

    Crud

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php include("db.php") ?>
+
+<?php include("includes/header.php") ?>
+
+<div class="container p-4">
+
+    <div class="row">
+
+        <div class="col-md-4">
+            <div class="card card-body">
+                <form action="save_task.php" method="POST">
+                    <div class="form-group p-2">
+                        <input type="text" name="title" class="form-control" placeholder="Task title" autofocus>
+                    </div>
+                    <div class="form-group p-2">
+                        <textarea name="description" rows="2" class="form-control" placeholder="Task description"></textarea>
+                    </div>
+                    <input type="submit" class="btn btn-success w-100" name="save_task" value="Save Task">    
+                </form>
+            </div>
+        </div>
+
+        <div class="col-md-8">
+        
+        </div>
+
+    
+    </div>
+
+
+</div>
+
+
+
+<?php include("includes/footer.php") ?>
+
+    
+
+
+
